fe49bfc70d397f540af5d0a20c2e8161d06c1a99
ey1031 <eyudeveloper@gmail.com>
2018-01-18 09:24:09 -0800

    complete saving of statuses and proper editing

--- page-injector.js
+++ page-injector.js
@@ -14,6 +14,68 @@ for (let card of company_cards) {
 					   			  </div>"
 }
 
+// event listeners because chrome extensions don't allow inline js
+for (let button of document.getElementsByClassName('default_button')) {
+	button.addEventListener('click', function(event) {
+		const default_value = application_status(event.target.getElementsByTagName('a')[0].innerHTML).text
+		const parent = event.target.parentNode
+		event.target.outerHTML = "<input style = 'margin-top: 10px; height: 38px; text-align: center;' type = 'text' placeholder = '" + default_value + "'>"
+		parent.getElementsByTagName('input')[0].addEventListener('keypress', function(event) {
+			input_keypress(event)
+		})
+	})
+}
+
+function input_keypress(event) {
+	event.preventDefault()
+	console.log(event.keyCode)
+	switch(event.keyCode) {
+		case 110:
+			event.target.value = "N/A"
+			break;
+		case 115:
+			event.target.value = "SENT"
+			break;
+		case 105:
+			event.target.value = "INTERVIEWING"
+			break;
+		case 111:
+			event.target.value = "OFFER"
+			break;
+		case 100:
+			event.target.value = "DECLINED"
+			break;
+		case 13:
+			const parent = event.target.parentNode
+
+			let init = event.target.placeholder
+			if (init != event.target.value && init != '') {
+				init = event.target.value
+				set(parent.getElementsByTagName('h3')[0].innerHTML, init.toLowerCase())
+				const properties = application_status(parent.getElementsByTagName('h3')[0].innerHTML)
+				event.target.outerHTML = "<div class = 'default_button' style = '" + default_button_css + properties.css + "'>\
+					   					<a style = '" + default_button_text_css + "'>" + init + "</a>\
+					   			  </div>"
+			}
+			else {
+				event.target.outerHTML = "<div class = 'default_button' style = '" + default_button_css + "'>\
+					   					<a style = '" + default_button_text_css + "'>" + init + "</a>\
+					   			  </div>"
+			}
+
+			console.log(parent)
+			parent.getElementsByClassName('default_button')[0].addEventListener('click', function(event) {
+				const default_value = application_status(event.target.getElementsByTagName('a')[0].innerHTML).text
+				const parent = event.target.parentNode
+				event.target.outerHTML = "<input style = 'margin-top: 10px; height: 38px; text-align: center;' type = 'text' placeholder = '" + default_value + "'>"
+				parent.getElementsByTagName('input')[0].addEventListener('keypress', function(event) {
+					input_keypress(event)
+				})
+			})
+			break;
+	}
+}
+
 function set(key, value) {
 	localStorage[key] = value
 }
@@ -22,32 +84,24 @@ function get(key) {
 	return localStorage[key]
 }
 
-// set('key1', 'value5')
-
 function application_status(key) {
 	switch(get(key)) {
 		case undefined: 
-			console.log("UNDEFINED")
 			return {css  : 'color: red;',
 					text : 'N/A'}
 		case 'sent':
-			console.log("SENT")
 			return {css  : 'color: blue;',
-					text : 'N/A'}
+					text : 'SENT'}
 		case 'interviewing':
-			console.log("INTERVIEWING")
 			return {css  : 'color: orange;',
-					text : 'N/A'}
+					text : 'INTERVIEWING'}
 		case 'offer':
-			console.log("RECIEVED OFFER")
 			return {css  : 'color: green;',
-					text : 'N/A'}
+					text : 'OFFER'}
 		case 'declined':
-			console.log("DECLINED")
 			return {css  : 'color: red;',
-					text : 'N/A'}
+					text : 'DECLINED'}
 		default:
-			console.log("EXISTS")
 			return {css  : 'color: white;',
 					text : 'N/A'}
 	}
